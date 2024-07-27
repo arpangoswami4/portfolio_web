8f2cb91092b22f209c045965e35401d80d269a8f
Arpan Goswami <arpan.goswami21@gmail.com>
2024-07-27 16:48:50 +0530

    testimonial view fix

--- src/components/TestimonialTemplate.jsx
+++ src/components/TestimonialTemplate.jsx
@@ -3,31 +3,30 @@ import React from "react";
 
 const TestimonialTemplate = (testimonial) => {
   return (
-    <div className="flex flex-col overflow-y-auto h-[30rem] gap-4 border-2 p-6 text-center border-zinc-300 rounded-lg">
-      <div className="mb-3 flex justify-center">
-        {testimonial.picture && (
-          <img
-            src={testimonial.picture}
-            className="rounded-3xl object-cover w-36 h-36"
-            alt={testimonial.name}
-          />
-        )}
-        {!testimonial.picture && (
-          <Icon
-            icon={"el:user"}
-            className="rounded-3xl object-cover w-24 h-24"
-            alt={testimonial.name}
-          />
-        )}
-      </div>
-      <div>
-        <div className="mb-3">
-          <h4 className="mb-1 text-3xl">{testimonial.name}</h4>
-          {testimonial.headline && (
-            <h6 className="mb-1 text-lg">{testimonial.headline}</h6>
+    <div className="flex flex-col justify-center items-center overflow-y-auto h-[30rem] border-2 p-6 text-center gap-y-4 border-zinc-300 rounded-lg">
+        <div className="mb-1">
+          {testimonial.picture && (
+            <img
+              src={testimonial.picture}
+              className="rounded-3xl object-cover h-40 w-40"
+              alt={testimonial.name}
+            />
+          )}
+          {!testimonial.picture && (
+            <Icon
+              icon={"el:user"}
+              className="rounded-3xl object-cover w-full h-40"
+            />
           )}
         </div>
-        <p className="italic">{testimonial.quote}</p>
+      <div>
+        <div className="mb-3">
+          <h4 className="text-3xl">{testimonial.name}</h4>
+          {testimonial.headline && (
+            <h6 className="mb-1 text-base">{testimonial.headline}</h6>
+          )}
+        </div>
+        <p className="italic">"{testimonial.quote}"</p>
       </div>
     </div>
   );
