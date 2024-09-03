04cf26ec3df7323b75ec815379f4155baa240efb
Arpan Goswami <arpan.goswami21@gmail.com>
2024-09-04 01:34:24 +0530

    Text added

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ function App() {
         <Layout />
         {process.env.NODE_ENV === "production" &&
           location.pathname !== "/about" && (
-            <p className="text-black mb-3 w-2/3 m-auto text-center text-orange-500 text-sm">
-              All the backend server functionality might take some extra
-              time(2-3 minutes) in production as the free tier of render.com is used
-              which builds and runs the container from the codebase for
+            <p className="mb-3 w-2/3 m-auto text-center text-orange-600 text-sm">
+              All the backend server functionality like contact form,
+              testimonial submission and CV download might take some extra
+              time(2-3 minutes) in production as the free tier of render.com is
+              used which builds and runs the container from the codebase for
               every first request.
             </p>
           )}
